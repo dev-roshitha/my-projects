7f1f255d3b7669aba7d8cce5ab9d194fd4b1b6af
unknown <sajithroshitha98@gmail.com>
2024-03-02 00:24:57 +0530

    update counter

--- react-counter/src/App.js
+++ react-counter/src/App.js
@@ -1,14 +1,26 @@
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0)
+  let increase = () => {
+    setCount(count + 1)
+  }
+  let decrease = () => {
+    setCount(count - 1)
+  }
+  let reset = () => {
+    setCount(0)
+  }
+  
   return (
     <div className="App">
       <div className='count-wrapper'>
-        <div className='count-number'>0</div>
+        <div className='count-number'>{count}</div>
         <div className='count-controls'>
-          <button className='btn increate'>+</button>
-          <button className='btn reset'>Reset</button>
-          <button className='btn decrease'>-</button>
+          <button className='btn increate' onClick={increase}>+</button>
+          <button className='btn reset' onClick={reset} disabled={count === 0}>Reset</button>
+          <button className='btn decrease' onClick={decrease} disabled={count === 0}>-</button>
         </div>
       </div>
     </div>
